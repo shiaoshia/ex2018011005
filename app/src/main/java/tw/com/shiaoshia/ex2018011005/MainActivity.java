81c99bd6526b064528c850426675382157670efb
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-10 16:09:23 +0800

    抓取XML資料

--- app/src/main/java/tw/com/shiaoshia/ex2018011005/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011005/MainActivity.java
@@ -2,6 +2,26 @@ package tw.com.shiaoshia.ex2018011005;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +29,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click01(View v) {
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                String str_url="https://www.mobile01.com/rss/news.xml";
+                URL url = null;
+
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn =(HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(inputStream);
+                    BufferedReader br = new BufferedReader(isr);
+                    StringBuilder sb = new StringBuilder();
+                    String str;
+
+                    while ((str = br.readLine()) != null) {
+                        sb.append(str);
+                    }
+                    String str1 = sb.toString();
+                    Log.d("NET", str1); //印出所有內容
+                    final MyHandler dataHandler = new MyHandler();
+                    SAXParserFactory spf = SAXParserFactory.newInstance();
+                    SAXParser sp = spf.newSAXParser();
+                    XMLReader xr = sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(str1)));
+
+                    br.close();
+                    isr.close();
+                    inputStream.close();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
     }
 }
