b1d4a8672ff9e970449c4430dda87effc241235d
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-11 10:39:21 +0800

    改成用Menu秀出mobile01裡news.xml的Title, 將按鈕刪除

--- app/src/main/java/tw/com/shiaoshia/ex2018011005/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011005/MainActivity.java
@@ -3,6 +3,8 @@ package tw.com.shiaoshia.ex2018011005;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -37,63 +39,75 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.listVIew);
     }
 
-    public void click01(View v) {
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                String str_url="https://www.mobile01.com/rss/news.xml";
-                URL url = null;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-                try {
-                    url = new URL(str_url);
-                    HttpURLConnection conn =(HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.connect();
-                    InputStream inputStream = conn.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(inputStream);
-                    BufferedReader br = new BufferedReader(isr);
-                    StringBuilder sb = new StringBuilder();
-                    String str;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_reload:
+                new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        String str_url="https://www.mobile01.com/rss/news.xml";
+                        URL url = null;
 
-                    while ((str = br.readLine()) != null) {
-                        sb.append(str);
-                    }
-                    String str1 = sb.toString();
-                    Log.d("NET", str1); //印出所有內容
-                    final MyHandler dataHandler = new MyHandler();
-                    SAXParserFactory spf = SAXParserFactory.newInstance();
-                    SAXParser sp = spf.newSAXParser();
-                    XMLReader xr = sp.getXMLReader();
-                    xr.setContentHandler(dataHandler);
-                    xr.parse(new InputSource(new StringReader(str1)));
+                        try {
+                            url = new URL(str_url);
+                            HttpURLConnection conn =(HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+                            InputStream inputStream = conn.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(inputStream);
+                            BufferedReader br = new BufferedReader(isr);
+                            StringBuilder sb = new StringBuilder();
+                            String str;
 
-                    br.close();
-                    isr.close();
-                    inputStream.close();
+                            while ((str = br.readLine()) != null) {
+                                sb.append(str);
+                            }
+                            String str1 = sb.toString();
+                            Log.d("NET", str1); //印出所有內容
+                            final MyHandler dataHandler = new MyHandler();
+                            SAXParserFactory spf = SAXParserFactory.newInstance();
+                            SAXParser sp = spf.newSAXParser();
+                            XMLReader xr = sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(str1)));
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter = new ArrayAdapter<String>(MainActivity.this,
-                                    android.R.layout.simple_list_item_1,dataHandler.title);
-                            listView.setAdapter(adapter);
+                            br.close();
+                            isr.close();
+                            inputStream.close();
+
+                            //將Title顯示到ListVIew
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter = new ArrayAdapter<String>(MainActivity.this,
+                                            android.R.layout.simple_list_item_1,dataHandler.title);
+                                    listView.setAdapter(adapter);
+                                }
+                            });
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
                         }
-                    });
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }.start();
+                    }
+                }.start();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
